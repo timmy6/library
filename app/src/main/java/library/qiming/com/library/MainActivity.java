b37687bd7cddd224e43dadb97bedd6b6844e01c8
liuqiming <liuqiminggood@gmail.com>
2018-07-30 15:19:40 +0800

    first commit

---
+++ app/src/main/java/library/qiming/com/library/MainActivity.java
@@ -0,0 +1,13 @@
+package library.qiming.com.library;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
